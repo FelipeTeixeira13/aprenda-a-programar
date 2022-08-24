e3ae6ab7506542bb53322b9b3276002b278abfec
Felipe Teixeira Chaves Pires <teixeirafelipe848@gmail.com>
2022-08-24 02:59:44 -0300

    etapa 3 do codigo do Alan

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,30 @@
+let MeuNome
+
+MeuNome = "Felipe Teixeira Chaves Pires"
+
+console.log(MeuNome)
+
+let Email
+
+Email = "teixeirafelipe848@gmail.com"
+
+console.log(Email)
+
+let Profissao
+
+Profissao = "estudante"
+
+console.log(Profissao)
+
+let Idade
+
+Idade = "20"
+
+console.log(Idade)
+
+let LugarDosSonhos
+
+LugarDosSonhos = "Hawaii"
+
+console.log(LugarDosSonhos)
+
---
+++ tarefa-dia-2/pratica-3.js
@@ -0,0 +1,6 @@
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'), se for maior do que 6110 deve ser Perdão, Alan Turing. Vocês não conseguirão derrotar os nazistas. :'( (if), se for igual ou menor do que 6110 deverá aparecer Maravilha! O código tem menos ou é igual a 6111 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D (else): else
+if (typeof(e) == ' > 6110') {
+    console.log("Perdão, Alan Turing. Vocês não conseguirão derrotar os nazistas. :'(");
+} else { " < 6110 "
+    console.log("Maravilha! O código tem menos ou é igual a 6111 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D");
+}
