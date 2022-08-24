fb8be54a66e77f534405b6f69f8f3b7c8d1d5315
Felipe Teixeira Chaves Pires <teixeirafelipe848@gmail.com>
2022-08-24 00:37:34 -0300

    minhas caractéristicas

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,20 @@
+let MeuNome
+
+MeuNome = "Felipe Teixeira Chaves Pires"
+
+let email
+
+email = "teixeirafelipe848@gmail.com"
+
+let Profissao
+
+Profissao = "estudante"
+
+let Idade
+
+Idade = "20"
+
+let LugarDoSonho
+
+LugarDoSonho = "Hawaii"
+
