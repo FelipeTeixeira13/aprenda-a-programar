78639226f2a00e554acc845bb3ea249f0ab9df56
Felipe Teixeira Chaves Pires <teixeirafelipe848@gmail.com>
2022-08-24 01:03:49 -0300

    tentativa das variaveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,11 +1,19 @@
 let MeuNome
 
+MeuNome = "Felipe Teixeira Chaves Pires"
+
 let email
+
+email = "teixeirafelipe848@gmail.com"
 
 let Profissao
 
+Profissao = "estudante"
+
 let Idade
+
+Idade = "20"
 
 let LugarDoSonho
 
-
+LugarDoSonho = "Hawaii"
