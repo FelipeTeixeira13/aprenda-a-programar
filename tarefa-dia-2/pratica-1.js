173074a8b224f9b620b40f5d1afe884ff25bcd2f
Felipe Teixeira Chaves Pires <teixeirafelipe848@gmail.com>
2022-08-24 00:49:27 -0300

    estou com muito frio

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,20 +1,11 @@
 let MeuNome
 
-MeuNome = "Felipe Teixeira Chaves Pires"
-
 let email
-
-email = "teixeirafelipe848@gmail.com"
 
 let Profissao
 
-Profissao = "estudante"
-
 let Idade
-
-Idade = "20"
 
 let LugarDoSonho
 
-LugarDoSonho = "Hawaii"
 
