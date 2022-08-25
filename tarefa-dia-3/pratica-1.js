4015cbac7c2f53348db6b589157eb405e031ae97
Felipe Teixeira Chaves Pires <teixeirafelipe848@gmail.com>
2022-08-25 19:06:13 -0300

    problema 1

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,8 @@
+// Exiba todos os números de 1 a n. Por exemplo, se o valor de n for 9, você deverá exibir o seguinte resultado na tela com console.log()
+
+let contador = 1
+
+while (contador <= 9 ) {
+    console.log (contador)
+    contador = contador + 1
+}
