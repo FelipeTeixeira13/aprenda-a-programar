5b13b1633e60c3c7b63d43b7ae596dd4477aa8f4
Felipe Teixeira Chaves Pires <teixeirafelipe848@gmail.com>
2022-08-25 21:57:36 -0300

    pratica2

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,17 @@
+// Fazendo a tabuada
+
+//9 x 1 = 9
+// 9 x 2 = 18
+// 9 x 3 = 27
+//
+
+let multiplicador = 9
+let n = 9
+let i = 1
+
+while ( i <= n) {
+let resultado = multiplicador * i
+let ValorDaTabuada = '${multiplicador} X${i} = $ {resultado}'
+console.log (resultado)
+i++
+}
